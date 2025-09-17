b63fdd438c60fa25dccc887a2207329865cc1e82
eufrat <lukzyw@gmail.com>
2025-09-17 23:32:41 +0200

    Add eureka client

--- parent/order-service/src/main/java/com/eufrat/order_service/OrderServiceApplication.java
+++ parent/order-service/src/main/java/com/eufrat/order_service/OrderServiceApplication.java
@@ -2,6 +2,7 @@ package com.eufrat.order_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 public class OrderServiceApplication {
