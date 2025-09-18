e532617ed01fd021a9f437d3065b8d706ff694f5
eufrat <lukzyw@gmail.com>
2025-09-18 11:28:42 +0200

    User and pass as env var

--- parent/discovery-server/src/main/java/com/eufrat/discoveryserver/config/SecurityConfig.java
+++ parent/discovery-server/src/main/java/com/eufrat/discoveryserver/config/SecurityConfig.java
@@ -1,26 +1,27 @@
 package com.eufrat.discoveryserver.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Value("${eureka.user}")
+    private String username;
+    @Value("${eureka.password}")
+    private String password;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -35,8 +36,8 @@ public class SecurityConfig {
 
     @Bean
     public UserDetailsService users() {
-        var user = User.withUsername("user")
-                .password(passwordEncoder().encode("password"))
+        var user = User.withUsername(username)
+                .password(passwordEncoder().encode(password))
                 .authorities("USER")
                 .build();
 
