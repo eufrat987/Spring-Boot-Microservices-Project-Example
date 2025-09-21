14f7568887cb1ec4f01f2a93c0957c9d68b8e480
eufrat <lukzyw@gmail.com>
2025-09-21 23:17:21 +0200

    Add header and oidc cofig

--- front/src/app/app.config.ts
+++ front/src/app/app.config.ts
@@ -1,10 +1,29 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 import { routes } from './app.routes';
+import { provideAuth } from 'angular-auth-oidc-client';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(withInterceptorsFromDi()),
+
+    provideAuth({
+      config: {
+        authority: 'http://localhost:8181/realms/spring-microservices-security-realm',
+        redirectUrl: window.location.origin,
+        postLogoutRedirectUri: window.location.origin,
+        clientId: 'angular-client',
+        scope: 'openid profile offline_access',
+        responseType: 'code',
+        silentRenew: true,
+        useRefreshToken: true,
+        renewTimeBeforeTokenExpiresInSeconds: 30,
+      }
+    }),
+
+
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes)
---
+++ front/src/app/shared/header/header.ts
@@ -0,0 +1,38 @@
+import {Component, inject, OnInit} from '@angular/core';
+import {OidcSecurityService} from "angular-auth-oidc-client";
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  templateUrl: './header.html',
+  styleUrl: './header.css',
+})
+export class Header {
+  private readonly oidcSecurityService = inject(OidcSecurityService);
+  isAuthenticated = false;
+  username = "";
+  
+  ngOnInit(): void {
+    this.oidcSecurityService.isAuthenticated$.subscribe(
+      ({isAuthenticated}) => {
+        this.isAuthenticated = isAuthenticated;
+      }
+    )
+    this.oidcSecurityService.userData$.subscribe(
+      ({userData}) => {
+        this.username = userData.preferred_username
+      }
+    )
+  }
+  
+  login(): void {
+    this.oidcSecurityService.authorize();
+  }
+  
+  logout(): void {
+    this.oidcSecurityService
+      .logoff()
+      .subscribe((result) => console.log(result));
+  }
+}
+
